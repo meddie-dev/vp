93ee25ecda4017b8489c0e49b33d20951574d89c
meddie-dev <johnmeddiemariano@gmail.com>
2024-06-16 15:44:25 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <img style={{ width: '100px'}} src="./src/assets/black-man (1).png" alt="" />
+        <p>Hi Nigger!</p>
+        <audio src="./src/assets/Gangsta Rap -  Ni__a Ni__a Ni__a  [HQ].mp3" controls autoPlay loop></audio>
+      </div>
+    </>
+  )
+}
+
+export default App
